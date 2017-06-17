9ad022d1bb96ee8f86f8abf32858a690e2d4b6d4
David Purdum <purdum41@gmail.com>
2017-06-17 04:53:01 -0400

    Install browserify to use require for node.js

--- database/index.js
+++ database/index.js
@@ -15,7 +15,7 @@ var database = firebase.database();
 
 function writeUserData(userId, name, email) {
   firebase.database().ref('users/' + userId).set({
-    username: name,
-    email: email
+    username: "name",
+    email: "email"
   });
 }
